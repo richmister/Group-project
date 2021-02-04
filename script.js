1f8fc0c78f81f8e3bad0206f70067bdba80bcabb
richmister09 <richmister09@gmail.com>
2021-02-04 10:24:30 -0500

    javaday

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+let budgetTotal = document.querySelector(".budget total");
+let entetainment = document.querySelector(".entertainment");
+let budgetForm = document.querySelector(".budget-form");
+let balance = document.querySelector(".balance");
+let expenseForm = document.querySelector(".expense form");
+
+budgetForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let formData = new FormData(budgetForm);
+  let amount = formData.get("amount");
+  console.log(amount);
+  balance.textContent = `${amount}`;
+  budgetForm.reset();
+});
